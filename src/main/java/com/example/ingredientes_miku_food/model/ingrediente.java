4e00d3c432f06be18dae71582dd82183b339c152
escondido478 <cr.zeppelin@duocuc.cl>
2025-05-15 18:08:22 -0400

    fix unsaved

--- src/main/java/com/example/ingredientes_miku_food/model/ingrediente.java
+++ src/main/java/com/example/ingredientes_miku_food/model/ingrediente.java
@@ -1,5 +1,29 @@
 package com.example.ingredientes_miku_food.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Ingrediente")
+@Entity
 public class ingrediente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 20)
+    private String nombre;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private Double costoXunidad;
+
 
 }
---
+++ src/main/java/com/example/ingredientes_miku_food/repository/ingredienterepository.java
@@ -0,0 +1,8 @@
+package com.example.ingredientes_miku_food.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ingredienterepository {
+
+}

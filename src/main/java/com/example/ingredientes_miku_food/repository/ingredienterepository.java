8d16e4985979e866b29345913897c8ab59b8b869
escondido478 <cr.zeppelin@duocuc.cl>
2025-05-15 23:27:36 -0400

    +reepo

--- src/main/java/com/example/ingredientes_miku_food/repository/ingredienterepository.java
+++ src/main/java/com/example/ingredientes_miku_food/repository/ingredienterepository.java
@@ -1,8 +1,29 @@
 package com.example.ingredientes_miku_food.repository;
+import com.example.ingredientes_miku_food.model.ingrediente;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ingredienterepository {
+public interface ingredienterepository extends JpaRepository<ingrediente, Long>{
+
+    ingrediente agregarIngreediente(ingrediente nuevoIng);
+
+    List<ingrediente> listaIngredientes();
+
+    ingrediente buscarporID(Integer id);
+
+    String reducirStock(int cantidad, Integer idIngrediente);
+
+    String aumentarStock(int cantidad, Integer idIngrediente);
+
+    ingrediente buscarXnombre(String name);
+
+    String elimIngredienteID(Integer id);
+
+    String elimIngredientename(String name);
+
+    
 
 }
